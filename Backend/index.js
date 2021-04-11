19cf073119450c390b690ac3d94ff6e59ccc99bc
Hustcketlyj <40982662+Hustcketlyj@users.noreply.github.com>
2021-04-11 20:07:15 +0800

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -32,6 +32,6 @@ app.use('/api/canteen', require('./routes/api/canteen'))
 app.use('/api/login', require('./routes/api/login'))
 //register router
 app.use('/api/register', require('./routes/api/register'))
-
+app.use('/api/com', require('./routes/api/comment_back'))
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>console.log(`Serevr started on PORT ${PORT}`))
