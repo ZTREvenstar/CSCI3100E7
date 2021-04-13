e67e979e28cbd2606a59d2073214c833c700c4ea
1155124470 <1155124470@link.cuhk.edu.hk>
2021-04-14 00:59:37 +0800

    update canteen change password

--- Backend/routes/api/login.js
+++ Backend/routes/api/login.js
@@ -2,14 +2,14 @@ const express = require('express')
 const router = express.Router()
 const sqlQuery = require('../../db')
 
-router.get('/',(req,res)=>{
-    res.render('loginchoose')
-})
+// router.get('/',(req,res)=>{
+//     res.render('loginchoose')
+// })
 
 //useer login page
-router.get('/user', (req, res) => {
-    res.render('userlogin')
-})
+// router.get('/user', (req, res) => {
+//     res.render('userlogin')
+// })
 
 
 //deal with user login request
@@ -30,9 +30,9 @@ router.post('/user', async (req, res) => {
     //res.send("post method")
 })
 
-router.get('/canteen', (req, res) => {
-    res.render('canteenlogin')
-})
+// router.get('/canteen', (req, res) => {
+//     res.render('canteenlogin')
+// })
 
 
 //deal with user login request
@@ -53,6 +53,15 @@ router.post('/canteen', async (req, res) => {
     //res.send("post method")
 })
 
-
+router.put('/canteen',async(req,res)=>{
+    console.log(req.body)
+    let id = req.query.id
+    let username = req.body.username
+    let password = req.body.password
+    let strSql = "update canteen set name = '"+ username +"' , password = '"+ password +"' where id = '"+ id +"';"
+    let result = await sqlQuery(strSql)
+    console.log(result)
+    res.status(200).send()
+})
 
 module.exports = router
