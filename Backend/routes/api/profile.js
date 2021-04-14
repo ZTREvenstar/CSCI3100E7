c0821a9c5a6165e0084a693df9353f99ba6ab74a
DevonRayMun <1155124465@link.cuhk.edu.hk>
2021-04-14 16:29:40 +0800

    update

---
+++ Backend/routes/api/profile.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const sqlQuery = require('../../db')
+const multer = require('multer') 
+const upload = multer() 
+const bp = require('body-parser');
+var path = require("path"), fs = require("fs");
+
+const router = express.Router()
+
+var createFolder = function(folder){
+    try{
+        fs.accessSync(folder); 
+    }catch(e){
+        fs.mkdirSync(folder);
+    }  
+};
+
+var profilePicFolder = path.join(__dirname, '..', '..', '..','public','customer');
+
+createFolder(profilePicFolder);
+
+var profilePicStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, profilePicFolder);   
+    },
+    filename: function (req, file, cb) {
+		console.log("namingfiles");
+		let id = req.body.id;
+		console.log(id);
+		fs.unlink(profilePicFolder+"/"+id +".png", (err) => {
+			if (err) {
+			  console.error(err)
+			  return
+			}
+		})
+		var profilePicName = id +".png";
+        cb(null, profilePicName);  
+    }
+});
+
+var uploadProfilePic = multer({storage: profilePicStorage });
+//router.use('/',express.static('../../../client/Canteen Interface'))
+
+
+router.all('/*', (req, res, next) => {
+	/* set response header */
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST");
+	res.setHeader("Access-Control-Allow-Headers", "*");
+	//res.setHeader('Content-Type', 'application/json');
+	next();
+})
+
+
+
+router.get('/', (req, res)=> {
+	console.log(req.query.id);
+    //fs.readFile(path.join(__dirname, '..', '..', 'foo.bar'));
+    console.log(path.join(__dirname, '..', '..', '..','client','Personal Info','personal.html' ));
+    res.sendFile(path.join(__dirname, '..', '..', '..','client','Personal Info','personal.html' ),root=path.join(__dirname, '..', '..', '..','client','Personal Info'))
+})
+
+router.post('/updateInfo', async function(req,res){
+	console.log(req.body);
+	
+	//console.log(databody);
+	//Object.assign(profileInfo,req.body)
+	var strSql = "SELECT * FROM customer WHERE id!=? AND username=?"
+    let customer = await sqlQuery(strSql, 
+        [req.body['id'],req.body['username']]);
+	let found = customer && Object.keys(customer).length === 0 && customer.constructor === Object
+	if(!found){
+		console.log("updating");
+		strSql = "UPDATE customer SET username=? WHERE id=?"
+		sqlQuery(strSql, 
+			[req.body['username'],req.body['id']]);
+		console.log("updated");
+	}else{
+	}
+
+	res.send(req.body);
+})
+
+/*router.post('/updateProfilePic', uploadProfilePic.single('chooseProfilePic'), function(req,res){
+	var file = req.file;
+})*/
+
+router.post('/info',async function(req,res){
+	console.log("get profile info");
+	console.log(req.body);
+	//res.send(req.body);
+	var strSql = 'SELECT * FROM customer WHERE id=' + req.body.id;
+	//var strSql_2 = 'SELECT * FROM customer WHERE id!=' + req.body.id + ' AND username=' + req.body.username;
+	let customer = await sqlQuery(strSql);
+
+	console.log(customer);
+
+    res.json(customer);
+})
+
+router.get('/profilePic',function(req,res){
+	console.log("get profile picture");
+	var strSql = 'SELECT * FROM customer';
+	res.send(profilePic);
+})
+
+router.get('/testProfile',async function(req,res){
+	console.log("show testing data");
+	var strSql = 'SELECT * FROM customer';
+	let customer_list = await sqlQuery(strSql);
+	console.log(customer_list);
+
+    res.json(customer_list);
+})
+
+router.post('/updateProfilePic', uploadProfilePic.single('img'), function(req,res){
+	var file = req.file;
+	console.log(file)
+	console.log(req.body.id);
+	res.send('success');
+})
+
+module.exports = router;
---
+++ my-app/src/Profile.js
@@ -0,0 +1,310 @@
+import React, { useState } from 'react'
+import $ from "jquery"
+const URL = "http://localhost:5000";
+
+class Profile extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            section: 0,
+            username: '',
+            updated:0,
+        };
+    };
+
+    componentDidMount() {
+        var that = this
+        this.serverRequest = $.ajax({
+            url: 'http://localhost:5000/api/profile/info',
+            type:'POST',
+            data: JSON.stringify({'id':this.props.id}),
+            contentType: "application/json",
+            dataType:'JSON',
+            async:true,
+            success: function(data,status){
+                console.log("succeed");
+                console.log(data);
+                that.setState({
+                    username: data[0].username,
+                    id: data[0].id,
+                })
+            },
+            error:function(data,status){
+                console.log("error");
+                console.log(data);
+            }
+        });
+    }
+     
+    componentWillUnmount() {
+        this.serverRequest.abort();
+    }
+
+    handleClick=(index)=>{
+        if(this.state.section === index){
+            this.setState({
+                section: 0,
+            });
+            console.log('hide display');
+        } else{
+            this.setState({
+                section: index,
+            });
+            console.log('show display #'+index)
+        }
+    }
+
+    renderPic=()=>{
+        console.log("changing states");
+        this.setState({
+            updated: this.state.updated+1,
+        })
+    }
+
+    render(){
+        let display = null;
+        switch(this.state.section){
+            case 0:
+                display = <></>
+                break;
+            case 1:
+                display = <ChangeInfo id = {this.props.id}/>
+                break;
+            case 2:
+                display = <ChangePW id = {this.props.id}/>
+                break;
+            case 3:
+                display = <UploadPic id = {this.props.id} handleSuccess = {this.handleClick} handlePicChange = {this.renderPic}/>
+                break;
+            default:
+                display = <></>
+        }
+
+        return(
+            <>
+            <header className = "container">
+                <h1 className = "my-4">Personal Information</h1>
+            </header>
+            <main className = "container">
+                <div id = "profileInfo" className="col-lg-8 col-md-6">
+                    <table className="table">
+                        <tbody>
+                        <tr>
+                            <th scope="row">User ID: </th>
+                            <td id = "id">{this.props.id}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Profile Name: </th>
+                            <td id = "profileName">{this.state.username}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Email Address:</th>
+                            <td id = "email"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div id="profilePicture" className = "col-lg-4 col-md-6">
+                    <picture>
+                    <img id = "profilePic" src={URL+"/public/customer/"+ this.props.id + ".png?"+this.state.updated} className="img-thumbnail" style={{height: 200}} alt={"no profile pic"}></img>
+                    </picture>
+                </div>
+                <div id="buttons" className = "d-flex flex-wrap my-2" >
+                    <button type="button" className="btn btn-primary m-2" onClick = {(e)=>this.handleClick(1)} >change Personal Information</button>
+                    <button type="button" className="btn btn-primary m-2" onClick = {(e)=>this.handleClick(2)} >change Password</button>
+                    <button type="button" className="btn btn-primary m-2" onClick = {(e)=>this.handleClick(3)} >Upload Profile Pic</button>
+                </div>
+                {display}
+            </main>
+        </>
+        );
+    };
+}
+
+class ChangeInfo extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {email: '', username:''};
+    
+      //  this.handleChange = this.handleChange.bind(this);
+      //  this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit=(event)=>{
+        event.preventDefault();
+
+        alert('trying to submit a form');
+        this.serverRequest = $.ajax({
+            url: 'http://localhost:5000/api/profile/updateInfo',
+            type:'POST',
+            data: JSON.stringify({
+                'email':this.state.email,
+                'username':this.state.username,
+                'id':this.props.id,
+            }),
+            contentType: "application/json",
+            dataType:'JSON',
+            async:true,
+            success: function(data,status){
+                console.log("succeed");
+                console.log(data);
+            },
+            error:function(data,status){
+                console.log("error");
+                console.log(data);
+            }
+        });
+        
+    }
+
+    usernameChange=(event)=>{
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    emailChange=(event)=>{
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <form id = "changeInfo" method = "post" onSubmit = {this.handleSubmit}>
+                        <div className="form-group">
+                        <label htmlFor="InputProfileName">Profile Name</label>
+                        <input type="text" className="form-control" id = "username" name="username" placeholder="Enter New User Name" onChange = {this.usernameChange}></input>email
+                    </div>
+                    <div className="form-group">                            
+                        <label htmlFor="InputEmailAddress">Email address</label>
+                        <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email" onChange = {this.emailChange}></input>
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        );
+    };
+}
+
+class ChangePW extends React.Component{
+    render(){
+        return(
+            <div>
+                    <form id = "changePassword">
+                        <div className="form-group">
+                            <label htmlFor="InputPassword">Password</label>
+                            <input type="password" className="form-control" id="inputPassword" placeholder="Enter New Password"></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="ConfirmPassword">Password</label>
+                            <input type="password" className="form-control" id="confirmPassword" placeholder="Repeat New Password"></input>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div> 
+        );
+    };
+}
+
+class UploadPic extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { 
+            img:null,
+        };
+    
+      //  this.handleChange = this.handleChange.bind(this);
+      //  this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    imgChange=(event)=>{
+        this.setState({img: event.target.files[0]});
+        console.log(event.target.files[0]);
+    }
+    
+    handleSubmit=(event)=>{
+        event.preventDefault();
+
+        let file = this.state.img;
+        let formdata = new FormData();
+        formdata.append('id',this.props.id);
+        formdata.append('img', file);
+        console.log(formdata);
+        this.serverRequest = $.ajax({
+            url: 'http://localhost:5000/api/profile/updateProfilePic',
+            type:'POST',
+            data: formdata,
+            contentType: false, processData: false,
+            async:true,
+            success: (data)=>{
+                window.alert("succeeded, now updating parent component");
+                console.log(data);
+                this.props.handleSuccess(3)
+                this.props.handlePicChange();
+            },
+            error:function(data){
+                console.log("error");
+                console.log(data);
+            }
+        });
+    }
+
+
+    render(){
+        return(
+        <div>
+            <form id ="changeProfilePic" method="post" onSubmit = {this.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor ="chooseProfilePic">Choose Profile Pic</label>
+                    <input id="chooseProfilePic" name="chooseProfilePic" type="file" className="form-control-file" onChange = {this.imgChange}></input> 
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div> )        
+    }
+}
+
+/*
+<div className="row my-2" id = "Profile">
+
+                <div>
+                    <form id = "changeProfileInfo">
+                        <div className="form-group">
+                            <label htmlFor="InputProfileName">Profile Name</label>
+                            <input type="text" className="form-control" id = "profileName" name="profileName" placeholder="Enter New Profile Name"></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="InputEmailAddress">Email address</label>
+                            <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>  
+                <div>
+                    <form id = "changePassword">
+                        <div className="form-group">
+                            <label htmlFor="InputPassword">Password</label>
+                            <input type="password" className="form-control" id="InputPassword" placeholder="Enter New Password"></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="ConfirmPassword">Password</label>
+                            <input type="password" className="form-control" id="ConfirmPassword" placeholder="Repeat New Password"></input>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div> 
+                <div>
+                    <form id ="changeProfilePic" action="http://localhost:3000/updateProfilePic" method="post" enctype="multipart/form-data">
+                        <div className="form-group">
+                            <label htmlFor ="chooseProfilePic">Choose Profile Pic</label>
+                            <input id="chooseProfilePic" name="chooseProfilePic" type="file" className="form-control-file">   </input> 
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div> 
+
+ */
+export default Profile;
